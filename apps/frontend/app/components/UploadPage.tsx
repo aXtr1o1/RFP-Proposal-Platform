edf4a7a510658eb31668b0e9ea88230ab76873d9
sanjeevan <sanjeevanmanoranjan@gmail.com>
2025-09-28 19:43:57 +0530

    added pinggy.link

--- apps/frontend/app/components/UploadPage.tsx
+++ apps/frontend/app/components/UploadPage.tsx
@@ -445,6 +445,7 @@ const UploadPage: React.FC<UploadPageProps> = () => {
 
   const postUuidConfig = async (uuid: string, config: string) => {
     const res = await fetch(`https://ayysx-13-201-229-249.a.free.pinggy.link/api/initialgen/${uuid}`, {
+      //https://ayysx-13-201-229-249.a.free.pinggy.link/api/initialgen
 
     //const res = await fetch(`http://localhost:8000/initialgen/${uuid}`, {
       method: "POST",
@@ -795,7 +796,7 @@ const UploadPage: React.FC<UploadPageProps> = () => {
 
       setProcessingStage('Regenerating document...');
       const res = await fetch(`https://ayysx-13-201-229-249.a.free.pinggy.link/api/regenerate/${jobUuid}`, {
-      
+      //https://ayysx-13-201-229-249.a.free.pinggy.link/api/initialgen
       //const res = await fetch(`http://localhost:8000/regenerate/${jobUuid}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
