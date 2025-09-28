0ca71608b13e2c36eaaace875dc81db7ef07b9ca
sanjeevan <sanjeevanmanoranjan@gmail.com>
2025-09-28 20:17:01 +0530

    again changed the url frontend

--- apps/frontend/app/components/UploadPage.tsx
+++ apps/frontend/app/components/UploadPage.tsx
@@ -444,8 +444,8 @@ const UploadPage: React.FC<UploadPageProps> = () => {
 
 
   const postUuidConfig = async (uuid: string, config: string) => {
-    const res = await fetch(`https://ayysx-13-201-229-249.a.free.pinggy.link/api/initialgen/${uuid}`, {
-      //https://ayysx-13-201-229-249.a.free.pinggy.link/api/initialgen
+    const res = await fetch(`https://zqcxy-13-201-229-249.a.free.pinggy.link/api/initialgen/${uuid}`, {
+
 
     //const res = await fetch(`http://localhost:8000/initialgen/${uuid}`, {
       method: "POST",
@@ -795,7 +795,7 @@ const UploadPage: React.FC<UploadPageProps> = () => {
       }
 
       setProcessingStage('Regenerating document...');
-      const res = await fetch(`https://ayysx-13-201-229-249.a.free.pinggy.link/api/regenerate/${jobUuid}`, {
+      const res = await fetch(`https://zqcxy-13-201-229-249.a.free.pinggy.link/api/regenerate/${jobUuid}`, {
       //https://ayysx-13-201-229-249.a.free.pinggy.link/api/initialgen
       //const res = await fetch(`http://localhost:8000/regenerate/${jobUuid}`, {
         method: "POST",
