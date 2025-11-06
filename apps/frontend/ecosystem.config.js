0a8f86ff666a56b24d3c5a87f1aa25b83e67ffaa
dharani-eswaramurthi <dharani96556@gmail.com>
2025-11-07 00:03:27 +0530

    vm deployment

--- apps/frontend/ecosystem.config.js
+++ apps/frontend/ecosystem.config.js
@@ -1,16 +1,6 @@
 module.exports = {
   apps: [
     {
-      name: "frontend-build",
-      cwd: "./",
-      script: "npm",
-      args: "run build",  // Run the 'build' script defined in your package.json
-      autorestart: false,   // Don't restart after exit
-      watch: false,         // Don't watch files
-      instances: 1,
-      exec_mode: "fork",    // Just run once
-    },
-    {
       name: "frontend-app",
       cwd: "./",
       script: "npm",
